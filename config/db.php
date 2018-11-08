4f162d3fea2d2ab0ecfd17347937e37edd58e30f
zoe606 <rive99erlangga@gmail.com>
2018-11-08 21:42:49 +0700

    initial push

---
+++ config/db.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=127.0.0.1;dbname=jobbase',
+    'username' => 'librantara',
+    'password' => 'nopass9',
+    'charset' => 'utf8',
+
+    // Schema cache options (for production environment)
+    //'enableSchemaCache' => true,
+    //'schemaCacheDuration' => 60,
+    //'schemaCache' => 'cache',
+];
